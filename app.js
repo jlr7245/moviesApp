9c1a69786aad0bec0f207bec70c13062dc8604ba
Karthik Meda <karthmeda@gmail.com>
2017-01-31 16:21:07 -0500

    Adding directors functions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -18,6 +19,9 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/directors',require('./routes/directors'))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/directors.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../db/models/index');
+
+
+
+// New director
+router.get('/new', function(req, res, next) {
+  res.render('directors/new', { title: 'new director' });
+});
+
+router.get('/', function(req, res, next) {
+  models.Director.findAll({}).then(function(directors) {
+    res.render('directors/index', {
+      title: 'directors',
+      directors: directors
+    });
+  });
+});
+
+//Posts data from input form to database
+router.post('/', function(req, res, next) {
+  models.Director.create({
+    name: req.body.fullName,
+  }).then(function() {
+    res.redirect('/directors')
+  });
+});
+
+//route to specify what happens on press of delete submit button
+router.delete('/:id', function(req, res, next) {
+  models.Director.destroy({
+    where: { id: req.params.id }
+  }).then(function(director) {
+    res.redirect('/directors');
+  });
+});
+
+
+//routes user to full director profile info on click of Director Name link on main page
+router.get('/:id', function(req, res, next) {
+  models.Director.findById(req.params.id).then(function(director) {
+    res.render('directors/show', { director: director, title: director.fullName });
+  });
+});
+
+//routes to edit view with input fields prepopulated with previously inputted information
+router.get('/:id/edit', function(req, res, next) {
+  models.Director.findById(req.params.id).then(function(director) {
+    res.render('directors/edit', { director: director });
+  });
+});
+
+//this function handles what happens on submit of editing director information
+router.put('/:id', function(req, res, next) {
+  models.Director.update({
+  name:req.body.fullName
+  }, { where: { id: req.params.id } }).then(function() {
+    res.redirect('/directors/');
+  });
+});
+
+
+module.exports = router;
