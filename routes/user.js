ed2c0e390117c6a9f3191c5836aff66099ea0ad8
J Silverstein <jess.pebble@gmail.com>
2017-01-31 11:32:52 -0500

    fixing errors

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var authHelpers = require('../auth/auth-helpers');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
