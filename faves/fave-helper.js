141db895cc94fccdd2bd440af343a0432992ca11
J Silverstein <jess.pebble@gmail.com>
2017-02-01 16:15:03 -0500

    favorites render on page

--- faves/fave-helper.js
+++ faves/fave-helper.js
@@ -1,10 +1,13 @@
 const models = require('../db/models/index');
 
 function getFavorites(req,res,next) {
-  models.sequelize.query('SELECT "Movies"."title" FROM "Movies" JOIN "Favorites" ON "Favorites"."movieId" = "Movies"."id" JOIN "Users" ON "Favorites"."userId" = "Users"."id" WHERE "Users"."id" = :id', {
-    replacements: { id: req.user.id }
-  }).spread((faves, meta) => console.log("here are the faves" + faves + "here is the meta" + meta));
-  return next();
+  models.sequelize.query('SELECT "Movies"."title", "Movies"."id", "Favorites"."createdAt" FROM "Movies" JOIN "Favorites" ON "Favorites"."movieId" = "Movies"."id" JOIN "Users" ON "Favorites"."userId" = "Users"."id" WHERE "Users"."id" = :id', {
+    replacements: { id: req.user.id },
+    type: models.sequelize.QueryTypes.SELECT
+  }).then((faves) => {
+    res.faves = faves;
+    return next();
+  });
 }
 
 module.exports = getFavorites;
