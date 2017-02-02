6900443a3c0b08caef801ff390ca74719efa9cc1
J Silverstein <jess.pebble@gmail.com>
2017-02-02 15:53:57 -0500

    commenting favhelper

--- faves/fave-helper.js
+++ faves/fave-helper.js
@@ -1,13 +1,13 @@
-const models = require('../db/models/index');
+const models = require('../db/models/index'); // importing the model
 
 function getFavorites(req,res,next) {
   models.sequelize.query('SELECT "Movies"."title", "Movies"."id", "Favorites"."createdAt" FROM "Movies" JOIN "Favorites" ON "Favorites"."movieId" = "Movies"."id" JOIN "Users" ON "Favorites"."userId" = "Users"."id" WHERE "Users"."id" = :id', {
-    replacements: { id: req.user.id },
-    type: models.sequelize.QueryTypes.SELECT
+    replacements: { id: req.user.id }, /// replaces :id in the query
+    type: models.sequelize.QueryTypes.SELECT // don't need metadata in the response
   }).then((faves) => {
-    res.locals.faves = faves;
-    return next();
+    res.locals.faves = faves; // setting res.locals object to access in the response
+    return next(); // next function
   });
 }
 
-module.exports = getFavorites;
+module.exports = getFavorites; // exporting the function
